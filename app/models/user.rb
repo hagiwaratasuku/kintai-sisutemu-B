e2e1d9c9003a598a8d3a1f737c1414a4144e7352
萩原翼 <ingakisan.6.2.2.4@ezweb.ne.jp>
2020-10-19 15:59:22 +0000

    本格的なログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   
-  # 入力されたメールアドレスを小文字に自動変換する
-  before_save { self.email = email.downcase } 
+  attr_accessor :remember_token   # 「remember_token」という仮想の属性を作成します。
+  before_save { self.email = email.downcase }   # 入力されたメールアドレスを小文字に自動変換する
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,4 +10,32 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  def User.digest(string)  # 渡された文字列のハッシュ値を返します。
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token  # ランダムなトークンを返します。
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember   # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)  # トークンがダイジェストと一致すればtrueを返します。
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget  # ユーザーのログイン情報を破棄します。
+    update_attribute(:remember_digest, nil)
+  end
 end
